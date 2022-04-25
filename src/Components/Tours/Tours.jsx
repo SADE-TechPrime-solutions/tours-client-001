f74aab0a8a659997914aab9945fa20592b9e604a
codeme254 <otwomadennis365@gmail.com>
2022-04-25 18:26:06 +0300

    continued adding client data

--- src/Components/Tours/Tours.jsx
+++ src/Components/Tours/Tours.jsx
@@ -13,7 +13,7 @@ const Tours = () => {
   const tours = [
     {
       image: tourImg1,
-      duration: "5 days trip",
+      duration: "5 days of trek",
       zone: "mount kenya",
       pricing: "from $1000",
       description:
@@ -21,7 +21,7 @@ const Tours = () => {
     },
     {
       image: tourImg2,
-      duration: "7 days hike",
+      duration: "7 days of trek",
       zone: "mount kilimanjaro",
       pricing: "from $1600",
       description:
@@ -29,43 +29,42 @@ const Tours = () => {
     },
     {
       image: tourImg3,
-      duration: "6 days trip",
-      zone: "nothern kenya special",
+      duration: "8 days safari",
+      zone: "nothern kenya",
       pricing: "from $2600",
-      description:
-        "",
+      description:"Get off the trodden tourist tracks on this camping expedition to remote, isolated and wild parts of northern Kenya. Journey along rough African roads to picturesque Lake Turkana and the stark Chalbi Desert then lush Marsabit National Park and the game-rich Samburu National Reserve, travelling by 4x4 Land Cruiser or overland truck.",
     },
     {
       image: tourImg4,
-      duration: "22 september - 30 september 2022",
-      zone: "l. victoria",
-      pricing: "from $15",
+      duration: "6-7 days trek",
+      zone: "mount ruwenzori",
+      pricing: "from $1900",
       description:
-        "we explore the luo land in kenya as we swim in lake victoria. The museum probably will be the best place you will visit here.",
+        "The Rwenzori Mountains is one of the top destinations visited by travelers that wish to explore the snow-capped mountains within Africa. Trekking the green, mist-shrouded Rwenzori Mountains is like stepping into a wonderland. An incredible experience will stay with you for a lifetime when a person talks about the Rwenzoris",
     },
     {
       image: tourImg6,
-      duration: "20 october 2022 - 1 november 2022",
-      zone: "l. nakuru",
-      pricing: "from $20",
+      duration: "6 days safari",
+      zone: "maasai mara",
+      pricing: "from $1000",
       description:
-        "If you have never seen flamingoes, then this is an opportunity that you don't want to waste, Lake Nakuru national park is where we will spend most of our time here.",
+        "The Maasai Mara requires no introduction. It is home to the wildebeest migration that takes place between it and the Serengeti every year. Other than the migration, the park is home to lions, buffalos, elephants, cheetahs, hippos, giraffes and hundreds of different animal species and birds. It is also one of the few places where you can do game drives and go for cultural tours in a Masai village.",
     },
     {
       image: tourImg5,
-      duration: "18 december 2022 - 3 january 2023",
-      zone: "mombasa",
-      pricing: "from $300",
+      duration: "5 days safari",
+      zone: "kenyan coast",
+      pricing: "from $2000",
       description:
-        "of course, no touring year can be complete if you fail to visit the coastal beach of Kenya known as Mombasa, let's explore the salty water and have more fun.",
+        "Any visitor to Kenya should consider visiting Mombasa during their holidays! This top destination for people who want to experience a tropical feel while in Africa. Mombasa provides the opportunity to enjoy incredible weather, the beautiful waters of the Indian Ocean, white sandy beaches, and the tastiest Kenyan food.",
     },
   ];
   return (
     <section className="tours" id="upcoming-tours">
       <Heading
-        subHeading="our tours"
-        mainHeading="see incoming tours"
-        bgText="book a tour"
+        subHeading="the best of east africa"
+        mainHeading="what we do best"
+        bgText="read more"
       />
       <div className="tours__container">
         {tours.map((el, i) => (
